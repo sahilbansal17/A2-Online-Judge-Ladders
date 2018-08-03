b63e97197fecd3bb798c139881bb59ce729d47eb
Sahil <shlbnsl843@gmail.com>
2018-08-03 22:50:06 +0530

    Add Div 2 D ladder problems done this week

---
+++ Div-2-D/7-489D.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Template file for Online Algorithmic Competitions */
+/* Typedefs */
+    /* Basic types */
+    typedef long long           ll;
+    typedef long double         ld;
+    typedef unsigned long long ull;
+    /* STL containers */
+    typedef vector <int>    vi;
+    typedef vector <ll>     vll;
+    typedef pair <int, int> pii;
+    typedef pair <ll, ll>   pll;
+    typedef vector < pii >  vpii;
+    typedef vector < pll >  vpll;
+    typedef vector <string> vs;
+    typedef vector < vi >   vvi;
+    typedef vector < vll >  vvll;
+    typedef vector < vpii > vvpii;
+    typedef set <int>       si;
+typedef complex <double>    pnt;
+typedef vector <pnt>        vpnt;
+typedef priority_queue <pii, vpii, greater<pii> > spq;
+/* Macros */
+    /* Loops */
+    #define fl(i, a, b)     for(int i(a); i < (b); i ++)
+    #define rep(i, n)       fl(i, 0, n)
+    #define rep1(i, n)      fl(i, 1, n)
+    #define rfl(i, a, b)    for(int i(a); i >= (b); i --)
+    /* Algorithmic functions */
+    #define srt(v)          sort((v).begin(), (v).end())
+    /* STL container methods */
+    #define pb              push_back
+    #define mp              make_pair
+    #define eb              emplace_back
+    #define mt              make_tuple
+    /* String methods */
+    #define slen(s)         s.length()
+    /* Shorthand notations */
+    #define F               first
+    #define S               second
+    #define re              return 
+    #define sz(x)           ((int) (x).size())
+    #define all(x)          (x).begin(), (x).end()
+    #define sqr(x)          ((x) * (x))
+    #define fill(x, y)      memset(x, y, sizeof(x))
+    #define endl            '\n'
+    /* Mathematical */
+    #define INF             0x3f3f3f3f
+    #define LLINF           1000111000111000111LL
+    #define PI              3.14159265358979323
+    /* Debugging purpose */
+    #define trace1(x)                cerr << #x << ": " << x << endl
+    #define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl
+    #define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl
+    #define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl
+    #define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl
+    #define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl
+/* Constants */
+    const ll MOD = 1000000007LL;
+    const ll MAX = 10010LL;
+/* Templates */
+template<class T> T abs(T x) { re x > 0 ? x : -x; }
+template<typename T> T gcd(T a, T b){ if(b == 0) return a; return gcd(b, a % b); }
+template<typename T> T power(T x, T y, ll m = MOD){T ans = 1; while(y > 0){ if(y & 1LL) ans = (ans * x)%m; y >>= 1LL; x = (x*x)%m; } return ans%m; }
+
+vi adj_list[3005];
+bool adj_matrix[3005][3005];
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/Users/sahilbansal/Desktop/input.txt","r",stdin);
+    freopen("/Users/sahilbansal/Desktop/output.txt","w",stdout);
+    #endif
+
+    int nodes, edges, x, y;
+    cin >> nodes >> edges;
+
+    rep(i, edges){
+        cin >> x >> y;
+        x --; // zero-based indexing
+        y --;
+        adj_list[x].pb(y);
+        adj_matrix[x][y] = true;
+    }
+
+    /*
+            b
+        a        c
+            d
+
+        This is a damn rhombi where a->b, b->c, a->d and d->c.
+
+        For all possible (a, c), we count the no of possible nodes that 
+        can be either b or d, let that count be r, 
+        so simply add (r choose 2) to the final result
+    */
+
+    ll result = 0, current;
+    rep(a, nodes){
+        rep(c, nodes){
+            if(a != c){
+                current = 0;
+                rep(index, sz(adj_list[a])){
+                    int next = adj_list[a][index];
+                    if(next != c && adj_matrix[next][c]){
+                        current ++;
+                    }
+                }
+                if(current & 1){
+                    result += ((current - 1)/2)*current;
+                }
+                else{
+                    result += (current/2)*(current - 1);
+                }
+            }
+        }
+    }
+
+    cout << result;
+
+    return 0;
+}
---
+++ Div-2-D/9-246D.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Template file for Online Algorithmic Competitions */
+/* Typedefs */
+    /* Basic types */
+    typedef long long           ll;
+    typedef long double         ld;
+    typedef unsigned long long ull;
+    /* STL containers */
+    typedef vector <int>    vi;
+    typedef vector <ll>     vll;
+    typedef pair <int, int> pii;
+    typedef pair <ll, ll>   pll;
+    typedef vector < pii >  vpii;
+    typedef vector < pll >  vpll;
+    typedef vector <string> vs;
+    typedef vector < vi >   vvi;
+    typedef vector < vll >  vvll;
+    typedef vector < vpii > vvpii;
+    typedef set <int>       si;
+typedef complex <double>    pnt;
+typedef vector <pnt>        vpnt;
+typedef priority_queue <pii, vpii, greater<pii> > spq;
+/* Macros */
+    /* Loops */
+    #define fl(i, a, b)     for(int i(a); i < (b); i ++)
+    #define rep(i, n)       fl(i, 0, n)
+    #define rep1(i, n)      fl(i, 1, n)
+    #define rfl(i, a, b)    for(int i(a); i >= (b); i --)
+    /* Algorithmic functions */
+    #define srt(v)          sort((v).begin(), (v).end())
+    /* STL container methods */
+    #define pb              push_back
+    #define mp              make_pair
+    #define eb              emplace_back
+    #define mt              make_tuple
+    /* String methods */
+    #define slen(s)         s.length()
+    /* Shorthand notations */
+    #define F               first
+    #define S               second
+    #define re              return 
+    #define sz(x)           ((int) (x).size())
+    #define all(x)          (x).begin(), (x).end()
+    #define sqr(x)          ((x) * (x))
+    #define fill(x, y)      memset(x, y, sizeof(x))
+    #define endl            '\n'
+    /* Mathematical */
+    #define oo              0x3f3f3f3f
+    #define LLINF           1000111000111000111LL
+    #define PI              3.14159265358979323
+    /* Debugging purpose */
+    #define trace1(x)                cerr << #x << ": " << x << endl
+    #define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl
+    #define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl
+    #define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl
+    #define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl
+    #define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl
+/* Constants */
+    const ll MOD = 100000000LL;
+    const ll MAX = 10010LL;
+/* Templates */
+template<class T> T abs(T x) { re x > 0 ? x : -x; }
+template<typename T> T gcd(T a, T b){ if(b == 0) return a; return gcd(b, a % b); }
+template<typename T> T power(T x, T y, ll m = MOD){T ans = 1; while(y > 0){ if(y & 1LL) ans = (ans * x)%m; y >>= 1LL; x = (x*x)%m; } return ans%m; }
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("/Users/sahilbansal/Desktop/input.txt","r",stdin);
+    freopen("/Users/sahilbansal/Desktop/output.txt","w",stdout);
+    #endif
+
+    int nodes, edges, x, y, cx, cy, ans, max_color = 0, min_color = oo, max_diversity = 0;
+    cin >> nodes >> edges;
+
+    vi colors(nodes, 0);
+    rep(i, nodes){
+        cin >> colors[i];
+        if(colors[i] > max_color){
+            max_color = colors[i];
+        }
+        if(colors[i] < min_color){
+            min_color = colors[i];
+        }
+    }
+
+    set <int> color_diversity[100010];
+    rep(i, edges){
+        cin >> x >> y;
+        x --, y --;
+        cx = colors[x];
+        cy = colors[y];
+        if(cx != cy){
+            color_diversity[cx].insert(cy);
+            color_diversity[cy].insert(cx);
+        }
+    }
+
+    ans = min_color ; // initialize, when max_diversity = 0 => ans = min_color
+    rep1(i, max_color + 1){
+        if(sz(color_diversity[i]) > max_diversity){
+            max_diversity = sz(color_diversity[i]);
+            ans = i;
+        }
+    }
+    cout << ans;
+
+    return 0;
+}
